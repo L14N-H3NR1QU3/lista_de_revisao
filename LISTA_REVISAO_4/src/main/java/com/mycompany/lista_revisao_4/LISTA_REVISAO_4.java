275abeb6ecadace9ccb0bc3a804fc50f43ebae19
Lian <lian.h.silva@aluno.senai.br>
2024-10-23 19:48:42 -0300

    atividade 4

--- LISTA_REVISAO_4/src/main/java/com/mycompany/lista_revisao_4/LISTA_REVISAO_4.java
+++ LISTA_REVISAO_4/src/main/java/com/mycompany/lista_revisao_4/LISTA_REVISAO_4.java
@@ -8,9 +8,22 @@ package com.mycompany.lista_revisao_4;
  *
  * @author l.silva
  */
+import java.util.Scanner;
 public class LISTA_REVISAO_4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner s = new Scanner (System.in);
+        int num;
+        System.out.println("Digite um numero: ");
+        String resp = s.next();
+        try{
+            num = Integer.parseInt(resp);
+            for(int i = 1; i <= 10; i++){
+                System.out.println(num+" x "+i+" = "+(num*i));
+            }
+        }
+        catch(NumberFormatException e){
+            System.out.println("Numero invalido!");
+        }
     }
 }
