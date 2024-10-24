71f6bab62855db653a8316da2b0a5e96eaf993ab
Lian <lian.h.silva@aluno.senai.br>
2024-10-23 21:00:38 -0300

    atividade 7

--- LISTA_DE_REVISAO/src/main/java/lista_revisao_7/LISTA_REVISAO_7.java
+++ LISTA_DE_REVISAO/src/main/java/lista_revisao_7/LISTA_REVISAO_7.java
@@ -4,10 +4,49 @@
  */
 package lista_revisao_7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author l.silva
  */
 public class LISTA_REVISAO_7 {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        int tam = 0;
+        int val;
+        System.out.println("Digite o tamanho do inventario: ");
+        String resp = s.next();
+        try{
+            tam = Integer.parseInt(resp);
+        }
+        catch(NumberFormatException e){
+            System.out.println("Numero invalido!");
+        }
+        int inv[] = new int [tam];
+        for(int i = 0; i < tam; i++){
+            System.out.println("Digite os valores do inventario: ");
+            resp = s.next();
+            try{
+                inv[i] = Integer.parseInt(resp);
+            }
+            catch(NumberFormatException e){
+                System.out.println("Numero invalido!");
+                i--;
+            }
+        }
+        System.out.println("Digite o valor desejado: ");
+        resp = s.next();
+        try{
+            val = Integer.parseInt(resp);
+            for(int i = 0; i < tam; i++){
+                if(val == inv[i]){
+                    System.out.println("Valor encontrado "+inv[i]+"["+(i)+"]");
+                }
+            }
+        }
+        catch(NumberFormatException e){
+            System.out.println("Numero invalido!");
+        }
+    }
 }
