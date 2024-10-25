d7648d93f67e2d82751e73b17e1ce2c02a5e8ee2
Lian <lian.h.silva@aluno.senai.br>
2024-10-24 21:47:08 -0300

    Novas att

---
+++ LISTA_DE_REVISAO/src/main/java/lista_revisao_10/LISTA_REVISAO_10.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lista_revisao_10;
+
+/**
+ *
+ * @author l.silva
+ */
+public class LISTA_REVISAO_10 {
+    public static void main(String[] args) {
+        
+    }
+}
--- LISTA_DE_REVISAO/src/main/java/lista_revisao_9/LISTA_REVISAO_9.java
+++ LISTA_DE_REVISAO/src/main/java/lista_revisao_9/LISTA_REVISAO_9.java
@@ -4,19 +4,24 @@
  */
 package lista_revisao_9;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author l.silva
  */
 public class LISTA_REVISAO_9 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner (System.in);
         int menuVal[] = {10, 20, 15, 25, 30};
         String menuNome[] = {"Suco", "Hamburguer", "Refrigerante", "Bolo", "Prato feito"};
         String resp;
         int qtd = 0;
+        int valTotal = 0;
         System.out.println("Digite seu nome: ");
         String nome = s.next();
         System.out.println("Quantidade de itens: ");
@@ -27,19 +32,38 @@ public class LISTA_REVISAO_9 {
         catch(NumberFormatException e){
             System.out.println("Numbero invalido!");
         }
+        int esc[] = new int [qtd];
         for(int i = 0; i < qtd; i++){
-            int esc[] = new int [qtd];
             for(int j = 0; j < 5; j++){
-                System.out.println((j+1)+"-"+menuNome[j]+": R$"+menuVal[j]);
+                System.out.println((j+1)+"-"+menuNome[j]+": R$"+menuVal[j]+".00");
             }
             System.out.println("Escolha: ");
             resp = s.next();
             try{
                 esc[i] = Integer.parseInt(resp);
+                if(esc[i] <= 0 && esc[i] > 5){
+                    System.out.println("Numero invalido!");
+                    i--;
+                }
             }
             catch(NumberFormatException e){
                 System.out.println("Numero invalido!");
             }
         }
+        for(int i = 0; i < qtd; i++){
+            valTotal += menuVal[esc[i]-1];
+        }
+        JOptionPane.showMessageDialog(null, "Valor total da compra: R$"+valTotal+".00");
+        FileWriter arquivo = new FileWriter("Pedido "+nome);
+        PrintWriter gravar = new PrintWriter(arquivo);
+        gravar.printf("----------------------------\n");
+        gravar.printf("Nome do cliente: "+nome+"\n");
+        gravar.printf("----------------------------\n");
+        for(int i = 0; i < qtd; i++){
+            gravar.printf(menuNome[esc[i]]+" - "+menuVal[esc[i]]+"\n");
+        }
+        gravar.printf("----------------------------\n");
+        gravar.printf("Valor total: "+valTotal+"\n");
+        arquivo.close();
     }
 }
